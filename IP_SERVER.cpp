6458781e7ce01dc6aca527171fbcd79b5ba187c3
Roi lopez <64579967+roilopezz@users.noreply.github.com>
2023-02-26 21:37:11 +0200

    Update IP_SERVER.cpp

--- IP_SERVER.cpp
+++ IP_SERVER.cpp
@@ -1,5 +1,84 @@
-void CreateAndShowWindow(HINSTANCE hInstance, const char* className, const char* windowTitle, int width, int height);
-void ReceiveScreenData(SOCKET clientSock, HWND hWnd);
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+    switch (message) {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+void CreateAndShowWindow(HINSTANCE hInstance, LPCSTR className, LPCSTR windowName, int width, int height) {
+    // Register the window class
+    WNDCLASSEX wc = {};
+    wc.cbSize = sizeof(WNDCLASSEX);
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = className;
+    RegisterClassEx(&wc);
+
+    // Create the window
+    HWND hWnd = CreateWindowEx(
+        WS_EX_LAYERED | WS_EX_TRANSPARENT | WS_EX_TOOLWINDOW,
+        className,
+        windowName,
+        WS_POPUP,
+        0, 0, width, height,
+        NULL, NULL, hInstance, NULL);
+
+    if (hWnd == NULL) {
+        std::cout << "CreateWindowEx failed: " << GetLastError() << std::endl;
+        return;
+    }
+
+    // Make the window transparent and layered
+    SetLayeredWindowAttributes(hWnd, RGB(0, 0, 0), 0, LWA_COLORKEY);
+
+    // Show the window
+    ShowWindow(hWnd, SW_SHOW);
+}
+
+void ReceiveScreenData(SOCKET clientSock, HWND hWnd) {
+    // Allocate memory for the screen data buffer
+    const int bufferLength = SCREEN_WIDTH * SCREEN_HEIGHT * 3;
+    char* buffer = new char[bufferLength];
+
+    // Set up a device context for the server-side window
+    HDC hdc = GetDC(hWnd);
+
+    while (true) {
+        // Receive the screen data
+        int bytesReceived = recv(clientSock, buffer, bufferLength, 0);
+        if (bytesReceived == SOCKET_ERROR) {
+            std::cout << "recv failed: " << WSAGetLastError() << std::endl;
+            break;
+        }
+
+        if (bytesReceived == 0) {
+            std::cout << "Connection closed" << std::endl;
+            break;
+        }
+
+        // Iterate over the received screen data and set the pixel color on the server-side window
+        for (int y = 0; y < SCREEN_HEIGHT; y++) {
+            for (int x = 0; x < SCREEN_WIDTH; x++) {
+                int i = (y * SCREEN_WIDTH + x) * 3;
+                BYTE b = (BYTE)buffer[i];
+                BYTE g = (BYTE)buffer[i+1];
+                BYTE r = (BYTE)buffer[i+2];
+                SetPixel(hdc, x, y, RGB(r, g, b));
+            }
+        }
+    }
+
+    // Release the device context and free the buffer memory
+    ReleaseDC(hWnd, hdc);
+    delete[] buffer;
+}
+
+
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
