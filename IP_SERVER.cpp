563d91acf65ba6770fbb1787fef6a79cdc0915e8
Roi lopez <64579967+roilopezz@users.noreply.github.com>
2023-03-12 16:21:20 +0200

    Update IP_SERVER.cpp

--- IP_SERVER.cpp
+++ IP_SERVER.cpp
@@ -1,9 +1,68 @@
-int _tmain(int argc, _TCHAR* argv[])
+// GameGuardServer.cpp : Defines the entry point for the application.
+//
+
+#include "stdafx.h"
+#include "GameGuardServer.h"
+#include <CommCtrl.h>
+//#include "IP_SERVER.h"
+
+
+
+
+#include "stdafx.h"
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <ws2spi.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+#include <time.h>  // include time.h header
+
+
+#pragma comment(lib, "ws2_32.lib")
+
+
+	using namespace std;
+
+	#define SERVER_PORT 55445
+	#define HEARTBeat_PORT 55446
+
+	#define BUF_SIZE 4096  // block transfer size  
+	#define QUEUE_SIZE 100 
+	extern void ReadInis ( void );
+	extern void GetMACaddress(void);
+	extern void InitLogFile (void);
+	extern void PrintLog( char HostNameData[20] ,	char RemoteIP[17] , unsigned char MACData[6] , char User_Name[20] , char ban_status , char add_ban ) ;
+	extern void Print_Ban( char HostNameData[20] ,	char RemoteIP[17] , unsigned char MACData[6] , char User_Name[20] , char ban_status , char add_ban , char BanReason[30] ) ;
+	extern void Print_Ban_Logs( char HostNameData[20] ,	char RemoteIP[17] , unsigned char MACData[6] , char User_Name[20] , char ban_status , char add_ban , char BanReason[30] ) ;
+	extern char Check_Ban( char HostNameData[20] ,	char RemoteIP[17] , unsigned char MACData[6] , char User_Name[20] , char ban_status , char add_ban ) ;
+	extern int checkBan;
+	int AutoBanTime=2;
+	int didbanname='1';
+	int didlogtxt='1';
+	int didlogonscr='1';
+	extern int MacProtect;
+	char ServerVers[3]={0};
+
+	unsigned char MACData[6];
+	char HostNameData[20];
+	char RemoteIP[17];
+	char ban_status;
+	char User_Name[20]={0};
+	char add_ban;
+	char BanReason[30]={0};
+
+
+
+	std::vector<std::string> ipAddresses;
+	std::vector<std::string> users;
+	std::vector<std::string> hardwareIds;
+
+
+
+int ServerOne_tmain()
 {
-
-
-
-
 	int		b, l, on = 1;
 	char	recvbuf[100], banforever='0'; 
 	char	buf[100];
@@ -13,6 +72,8 @@ int _tmain(int argc, _TCHAR* argv[])
 	WSADATA wsaData;
 	int		err;
 	int		bytesRecv;
+
+	/*
 	unsigned char MACData[6];
 	char HostNameData[20];
 	char RemoteIP[17];
@@ -20,6 +81,8 @@ int _tmain(int argc, _TCHAR* argv[])
 	char User_Name[20]={0};
 	char add_ban;
 	char BanReason[30]={0};
+	*/
+
 	int i;
 	SYSTEMTIME st;
 
@@ -32,7 +95,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	//GetMACaddress();
 	ReadInis(); // Load Config
 	printf(" \n");
-	printf(" Users Connections : \n");
+	printf(" [ == Users Blocked == ] \n");
 	printf(" \n");
 
 
@@ -101,11 +164,15 @@ int _tmain(int argc, _TCHAR* argv[])
 	int clientAddrLen = sizeof(channel);
 	sa = accept(s, (SOCKADDR*)&channel, &clientAddrLen);
 
-	if (sa < 0) {
+	if (sa < 0 || sa == INVALID_SOCKET) {
 		printf("accept error %ld ", WSAGetLastError() ); 
 		WSACleanup();
-		return false;
+		//return false;
+		continue;
 	}
+
+
+
 	//------------------------------------------------------------
 	// Socket is now set up and bound. Wait for connection and process it. 
 	
@@ -156,6 +223,25 @@ int _tmain(int argc, _TCHAR* argv[])
 	GetLocalTime(&st);
 
 
+
+	// Get the client IP address
+	sockaddr_in saClient;
+	int nLength = sizeof(saClient);
+	getpeername(sa, (sockaddr*)&saClient, &nLength);
+	char szIPAddress[16];
+	inet_ntop(AF_INET, &saClient.sin_addr, szIPAddress, sizeof(szIPAddress));
+
+	// Get the user and hardware ID
+	char szUser[256] = "User1";
+	char szHardwareID[256] = "HWID1";
+
+	// Add the data to the vectors
+	ipAddresses.push_back(szIPAddress);
+	users.push_back(szUser);
+	hardwareIds.push_back(szHardwareID);
+
+
+
 	//if(didlogonscr==1 || add_ban=='1'){
 	if(add_ban=='1' || Check_Ban(HostNameData , RemoteIP , MACData , User_Name , ban_status , add_ban) ==  '1'){
 
@@ -169,7 +255,7 @@ int _tmain(int argc, _TCHAR* argv[])
 
 
 	}else{
-	printf(" -User connected : (%s), Date: %02d/%02d/%02d, Time: %02d:%02d:%02d, \n",RemoteIP, st.wDay , st.wMonth , st.wYear,st.wHour, st.wMinute, st.wSecond); //, //BanReason//ban_status)	
+	//printf(" -User connected : (%s), Date: %02d/%02d/%02d, Time: %02d:%02d:%02d, \n",RemoteIP, st.wDay , st.wMonth , st.wYear,st.wHour, st.wMinute, st.wSecond); //, //BanReason//ban_status)	
 	}
 
 
@@ -221,4 +307,264 @@ int _tmain(int argc, _TCHAR* argv[])
 	closesocket( s );
 	WSACleanup();
 	return 0;
+}
+
+
+DWORD WINAPI runHeartbeatServer(LPVOID lpParam)
+{
+    // Create a socket for the heartbeat server
+    SOCKET heartbeatSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (heartbeatSocket == INVALID_SOCKET) {
+        std::cerr << "Failed to create socket with error: " << WSAGetLastError() << std::endl;
+        return 1;
+    }
+
+    // Bind the socket to a local address and port for the heartbeat server
+    sockaddr_in heartbeatAddress;
+    heartbeatAddress.sin_family = AF_INET;
+    heartbeatAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+    heartbeatAddress.sin_port = htons(HEARTBeat_PORT);
+
+
+    int result = bind(heartbeatSocket, (SOCKADDR*)&heartbeatAddress, sizeof(heartbeatAddress));
+
+    if (result == SOCKET_ERROR) {
+        std::cerr << "Failed to bind socket with error: " << WSAGetLastError() << std::endl;
+        closesocket(heartbeatSocket);
+        return 1;
+    }
+
+    // Listen for incoming connections for the heartbeat server
+    result = listen(heartbeatSocket, SOMAXCONN);
+    if (result == SOCKET_ERROR) {
+        std::cerr << "Failed to listen for incoming connections with error: " << WSAGetLastError() << std::endl;
+        closesocket(heartbeatSocket);
+        return 1;
+    }
+
+    //std::cout << "Heartbeat server is listening for incoming connections..." << std::endl;
+
+    // Accept incoming connections and send heartbeat messages
+    while (true) {
+        SOCKADDR_IN clientAddress;
+        int clientAddressSize = sizeof(clientAddress);
+        SOCKET clientSocket = accept(heartbeatSocket, (SOCKADDR*)&clientAddress, &clientAddressSize);
+        if (clientSocket == INVALID_SOCKET) {
+            std::cerr << "Failed to accept incoming connection with error: " << WSAGetLastError() << std::endl;
+            closesocket(heartbeatSocket);
+            return 1;
+        }
+
+        //std::cout << "Heartbeat server accepted incoming connection from " << inet_ntoa(clientAddress.sin_addr) << std::endl;
+
+        // Send heartbeat messages to the client
+        const char* heartbeatMessage = "heartbeat";
+        while (true) {
+            result = send(clientSocket, heartbeatMessage, strlen(heartbeatMessage), 0);
+            if (result == SOCKET_ERROR) {
+           //     std::cerr << "Failed to send heartbeat message with error: " << WSAGetLastError() << std::endl;
+			closesocket(clientSocket);
+			break;
+			}
+	//std::cout << "Heartbeat message sent to " << inet_ntoa(clientAddress.sin_addr) << std::endl;
+	Sleep(1000); // Wait for 1 second before sending the next heartbeat message
+	}
+
+    closesocket(clientSocket);
+}
+
+// Close the heartbeat server socket
+closesocket(heartbeatSocket);
+
+return 0;
+}
+
+
+
+
+DWORD WINAPI RoieLopezzzzz(LPVOID lpParam)
+{
+    WSADATA wsaData;
+    int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (result != 0) {
+        std::cerr << "WSAStartup failed with error: " << result << std::endl;
+        return 1;
+    }
+
+    // Create two threads to run the heartbeat server and the other server concurrently
+    HANDLE heartbeatThread = CreateThread(NULL, 0, runHeartbeatServer, NULL, 0, NULL);
+    
+	
+	ServerOne_tmain();
+	
+	//HANDLE otherThread = CreateThread(NULL, 0, runOtherServer, NULL, 0, NULL);
+
+    // Wait for the threads to finish before exiting the program
+    WaitForSingleObject(heartbeatThread, INFINITE);
+   // WaitForSingleObject(otherThread, INFINITE);
+
+    // Close the thread handles
+    CloseHandle(heartbeatThread);
+    //CloseHandle(otherThread);
+
+    // Clean up Winsock
+    WSACleanup();
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+// Define the table control ID
+#define ID_TABLE 1001
+
+// Define the column headers
+LPCWSTR columnHeaders[] = {
+    L"IP",
+    L"Hardware ID",
+    L"Status"
+};
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
+{
+
+		//();
+	CreateThread(NULL, 0, RoieLopezzzzz, (LPVOID)1, 0, NULL);
+
+
+    // Register the window class
+    const wchar_t CLASS_NAME[]  = L"MyWindowClass";
+    
+    WNDCLASS wc = { };
+
+    wc.lpfnWndProc   = WindowProc;
+    wc.hInstance     = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+
+    RegisterClass(&wc);
+
+    // Create the window
+    HWND hwnd = CreateWindowEx(
+        0,
+        CLASS_NAME,
+        L"GameGuardServer",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, 500, 500,
+        NULL, NULL, hInstance, NULL
+    );
+
+    if (hwnd == NULL)
+    {
+        return 0;
+    }
+
+
+
+    // Create the table control
+    HWND hTable = CreateWindowEx(
+        0,
+        WC_LISTVIEW,
+        L"",
+        WS_CHILD | WS_VISIBLE | LVS_REPORT | LVS_SINGLESEL,
+        10, 10, 470, 450,
+        hwnd,
+        (HMENU)ID_TABLE,
+        hInstance,
+        NULL
+    );
+
+    // Set the column headers
+    LVCOLUMN lvColumn = { };
+    lvColumn.mask = LVCF_TEXT | LVCF_WIDTH;
+    lvColumn.cx = 100;
+
+    for (int i = 0; i < 3; i++)
+    {
+        lvColumn.pszText = (LPWSTR)columnHeaders[i];
+        SendMessage(hTable, LVM_INSERTCOLUMN, i, (LPARAM)&lvColumn);
+    }
+
+    // Add some items to the table
+    LVITEM lvItem = { };
+    lvItem.mask = LVIF_TEXT;
+
+  // Clear the table
+SendMessage(hTable, LVM_DELETEALLITEMS, 0, 0);
+
+// Add the data to the table
+//for (int i = 0; i < ipAddresses.size(); i++)
+
+for (int i = 0; i < ipAddresses.size(); i++)
+
+{
+    LVITEM lvItem;
+    lvItem.mask = LVIF_TEXT;
+    lvItem.iItem = i;
+    lvItem.iSubItem = 0;
+
+    // Convert narrow character string to wide character string
+    wchar_t ipAddressW[256];
+    MultiByteToWideChar(CP_ACP, 0, ipAddresses[i].c_str(), -1, ipAddressW, 256);
+    lvItem.pszText = ipAddressW;
+
+    SendMessage(hTable, LVM_INSERTITEM, 0, (LPARAM)&lvItem);
+
+    lvItem.iSubItem = 1;
+
+    // Convert narrow character string to wide character string
+    wchar_t userW[256];
+    MultiByteToWideChar(CP_ACP, 0, users[i].c_str(), -1, userW, 256);
+    lvItem.pszText = userW;
+
+    SendMessage(hTable, LVM_SETITEM, 0, (LPARAM)&lvItem);
+
+    lvItem.iSubItem = 2;
+
+    // Convert narrow character string to wide character string
+    wchar_t hardwareIdW[256];
+    MultiByteToWideChar(CP_ACP, 0, hardwareIds[i].c_str(), -1, hardwareIdW, 256);
+    lvItem.pszText = hardwareIdW;
+
+    SendMessage(hTable, LVM_SETITEM, 0, (LPARAM)&lvItem);
+}
+
+    // Show the window
+    ShowWindow(hwnd, nCmdShow);
+
+    // Run the message loop
+    MSG msg = { };
+
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+
+
+
+    return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+
+        default:
+            return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
 }
